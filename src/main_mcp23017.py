417cf68a4692634f0ae546e1cfdceca743218a59
Slavisa Blesic <slavisablesic@Slavisas-MacBook-Pro.local>
2024-06-06 23:41:10 +0200

    dd

--- src/main_mcp23017.py
+++ src/main_mcp23017.py
@@ -85,8 +85,9 @@ def set_pin_value(pin, value):
         pin_states[address][0 if device_pin < 8 else 1] |= (1 << pin_offset)
     else:
         pin_states[address][0 if device_pin < 8 else 1] &= ~(1 << pin_offset)
-
-    bus.write_byte_data(address, register, pin_states[address][0 if device_pin < 8 else 1])
+    vrednost = 0;
+    vrednost = pin_states[address][0 if device_pin < 8 else 1]
+    bus.write_byte_data(address, register, int(vrednost))
     print(f"Set pin {pin} (device {pin_mapping[pin]['name']} pin {device_pin}) to {'HIGH' if value == 1 else 'LOW'}")
 
 def get_pin(pin):
